5dce78bd47766b61713c8698aaa12608f6f0fdc3
Matthew Stengel <matthewstengel69@gmail.com>
2026-04-07 23:26:14 -0500

    redirect urls

--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -16,8 +16,10 @@ const shopify = shopifyApp({
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
+  isEmbeddedApp: true,
   future: {
     expiringOfflineAccessTokens: true,
+    unstable_newEmbeddedAuthStrategy: true,
   },
   ...(process.env.SHOP_CUSTOM_DOMAIN
     ? { customShopDomains: [process.env.SHOP_CUSTOM_DOMAIN] }
