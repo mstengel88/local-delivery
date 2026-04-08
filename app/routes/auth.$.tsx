20dcfaf911191f9d5bcaf13ed2dbe1183e87ba5e
Matthew Stengel <matthewstengel69@gmail.com>
2026-04-08 09:10:13 -0500

    routes

--- app/routes/auth.$.tsx
+++ app/routes/auth.$.tsx
@@ -1,14 +1,11 @@
-
-import type { HeadersFunction, LoaderFunctionArgs } from "react-router";
+import type { LoaderFunctionArgs } from "react-router";
 import { authenticate } from "../shopify.server";
-import { boundary } from "@shopify/shopify-app-react-router/server";
 
-export const loader = async ({ request }: LoaderFunctionArgs) => {
+export async function loader({ request }: LoaderFunctionArgs) {
   await authenticate.admin(request);
-
   return null;
-};
+}
 
-export const headers: HeadersFunction = (headersArgs) => {
-  return boundary.headers(headersArgs);
-};
+export default function AuthCatchAll() {
+  return null;
+}
