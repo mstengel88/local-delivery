f90c97a86d5fdf5a4ddfffda1ec623f8bceff165
Matthew Stengel <matthewstengel69@gmail.com>
2026-04-08 01:22:09 -0500

    restore app.tsx

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,21 @@
-import { flatRoutes } from "@react-router/fs-routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default flatRoutes();
+export default [
+  index("routes/_index/route.tsx"),
+
+  route("auth/login", "routes/auth.login/route.tsx"),
+  route("auth/*", "routes/auth.$.tsx"),
+
+  route("app", "routes/app.tsx", [
+    index("routes/app._index.tsx"),
+    route("admin", "routes/app.admin.tsx"),
+    route("additional", "routes/app.additional.tsx"),
+    route("custom-quote", "routes/app.custom-quote.tsx"),
+  ]),
+
+  route("api/shipping-estimate", "routes/api.shipping-estimate.ts"),
+  route("api/carrier-service", "routes/api.carrier-service.ts"),
+
+  route("webhooks/app/scopes_update", "routes/webhooks.app.scopes_update.tsx"),
+  route("webhooks/app/uninstalled", "routes/webhooks.app.uninstalled.tsx"),
+] satisfies RouteConfig;
