121a7bdd7e06846caff1b30636d527b4229917bc
Matthew Stengel <matthewstengel69@gmail.com>
2026-04-08 02:07:27 -0500

    restore app.tsx

--- app/routes/auth.login/route.tsx
+++ app/routes/auth.login/route.tsx
@@ -7,9 +7,11 @@ import { login } from "../../shopify.server";
 import { loginErrorMessage } from "./error.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const shop = url.searchParams.get("shop") || "";
   const errors = loginErrorMessage(await login(request));
 
-  return { errors };
+  return { errors, shop };
 };
 
 export const action = async ({ request }: ActionFunctionArgs) => {
@@ -23,25 +25,27 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 export default function Auth() {
   const loaderData = useLoaderData<typeof loader>();
   const actionData = useActionData<typeof action>();
-  const [shop, setShop] = useState("");
   const { errors } = actionData || loaderData;
+
+  const [shop, setShop] = useState(loaderData.shop || "");
 
   return (
     <AppProvider embedded={false}>
       <s-page>
         <Form method="post">
-        <s-section heading="Log in">
-          <s-text-field
-            name="shop"
-            label="Shop domain"
-            details="example.myshopify.com"
-            value={shop}
-            onChange={(e) => setShop(e.currentTarget.value)}
-            autocomplete="on"
-            error={errors.shop}
-          ></s-text-field>
-          <s-button type="submit">Log in</s-button>
-        </s-section>
+          <s-section heading="Log in">
+            <s-text-field
+              name="shop"
+              label="Shop domain"
+              details="example.myshopify.com"
+              value={shop}
+              onChange={(e) => setShop(e.currentTarget.value)}
+              autocomplete="off"
+              error={errors.shop}
+            ></s-text-field>
+
+            <s-button type="submit">Log in</s-button>
+          </s-section>
         </Form>
       </s-page>
     </AppProvider>
