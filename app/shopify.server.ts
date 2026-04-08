c748fb675d151479a7569847de8f17bb2bef2ba2
Matthew Stengel <matthewstengel69@gmail.com>
2026-04-08 16:52:05 -0500

    routes

--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -7,6 +7,10 @@ import {
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
 import prisma from "./db.server";
 
+console.log("SHOPIFY_APP_URL =", process.env.SHOPIFY_APP_URL);
+console.log("APP_URL =", process.env.APP_URL);
+console.log("HOST =", process.env.HOST);
+
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
